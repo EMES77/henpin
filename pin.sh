958fb09e453764105ede2a06b9afdf2b2b07f672
EMES77 <martin@kompakt-film.de>
2023-06-20 22:12:23 +0200

    Update pin.sh

--- pin.sh
+++ pin.sh
@@ -6,6 +6,8 @@ node pin.js tz1iyFi4WjSttoja7Vi1EJYMEKKSebQyMkF9
 node pin.js tz1c2e1WMKKtf6oBZcZK3YFxrXgVbb2ggRr9
 # @antic's kid => awesome abstracts
 node pin.js tz1Rwv7Kwy6SzAZasTcZKoXDf7jf8LE8WxiY
+# @cryptemes
+node pin.js tz1UecmgMghaWovhh9EYfqTrK9gTtaiMtbzs
 
 # add yours (and let @antic know you are using this tool so he can add you!)
 
